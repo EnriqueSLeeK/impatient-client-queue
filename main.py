b3edb885da13ce3a56623a90fe3d2fad8ad3b144
ensebast <enri_lee@usp.br>
2024-04-13 18:43:23 -0300

    initial parameter

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 
-def main():
+from json import load
+
+def main(config: dict, result: dict):
+    print(config)
     return
 
 if __name__ == "__main__":
-    main()
+    config = {}
+
+    result = {
+        "N_res": [],
+        "W_mean": [],
+        "W_cis": [],
+        "TM_means": [],
+        "TM_cis": [],
+        "W_values": [],
+        "TM_values": []
+    }
+
+    with open("config.json") as f:
+        config = load(f)
+    main(config)
