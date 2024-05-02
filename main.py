8a2b90914c0bf79e1c802b4e4a35ca98a9b4f2d8
ensebast <enri_lee@usp.br>
2024-05-02 16:08:47 -0300

    prototype

--- main.py
+++ main.py
@@ -1,23 +1,112 @@
 
-from json import load
+import json
+import numpy as np
 
-def main(config: dict, result: dict):
-    print(config)
-    return
+# Calculation Functions
+def not_served_proportion(served, forfeit, queue_len):
+    return (forfeit / (served + forfeit + queue_len))
 
-if __name__ == "__main__":
+def prob_forfeit(queue_len, cashier_quant):
+    return queue_len / (queue_len + cashier_quant)
+
+def std_error(variance, number_experiment):
+    return variance / np.sqrt(number_experiment)
+
+def calculate_confidence_interval(std_error):
+    return 2 * 1.96 * std_error
+
+# Simulation
+def simulate_one_instance(config: dict):
+
+    time = 0
+    forfeit = 0
+    # No one was served
+    served = -1
+    proportion = -1
+    waiting_max = -1
+
+    queue = []
+    curr_queue_size = 0
+    total_joined_queue = 0
+
+    cashier = [0 for _ in range(config['n'])]
+
+    while (True):
+        # Arrival interval
+        arrival_interval = np.random.exponential(config['lambda'])
+        tmp = time + arrival_interval
+
+        # Check to return
+        if (tmp > config['T']):
+            return {
+                'served': served,
+                'forfeit': forfeit,
+                'proportion': proportion,
+                'waiting_max': waiting_max
+            }
+
+        time = tmp
+        total_joined_queue += 1
+        queue.append(time)
+
+        lowest_cashier_time_index = np.argmin(cashier)
+
+        # Serving
+        while (cashier[lowest_cashier_time_index] <= time and
+                served < total_joined_queue):
+            served += 1
+            serving_interval = np.random.exponential(config['mu'])
+            cashier[lowest_cashier_time_index] = np.max([cashier[lowest_cashier_time_index], queue[served]]) + serving_interval
+            waiting_max = np.max([waiting_max, np.abs(cashier[lowest_cashier_time_index] - queue[served])])
+
+        curr_queue_size = np.max([0, (total_joined_queue - 1) - served])
+
+        # Check if the client left without joining
+        client_forfeit = np.random.binomial(n=1, p=prob_forfeit(queue_len=curr_queue_size,
+                                                                cashier_quant=config['n']))
+        if (client_forfeit == 1):
+            total_joined_queue -= 1
+            forfeit += 1
+
+        # Calculate the current queue size
+        curr_queue_size = total_joined_queue - served
+        proportion = not_served_proportion(served=served,
+                                           forfeit=forfeit,
+                                           queue_len=curr_queue_size)
+
+# Wrapper to append a element to an array
+def add_elem(target_array, data):
+    target_array.append(data)
+
+def main(config: dict):
+
+    served_list = []
+    forfeit_list = []
+    proportion_list = []
+    waiting_max_list = []
+    confidence_interval = 1
+    repeat_counter = 0
+
+    partial_mean_proportion_list = []
+    partial_mean_waiting_max_list = []
+
+    while (confidence_interval >= config['se_threshold']):
+        repeat_counter += 1
+        for _ in range(config['N']):
+            simulation_result = simulate_one_instance(config)
+            add_elem(served_list, simulation_result['served'])
+            add_elem(forfeit_list, simulation_result['forfeit'])
+            add_elem(proportion_list, simulation_result['proportion'])
+            add_elem(waiting_max_list, simulation_result['waiting_max'])
+
+        repeat_counter += 1
+        confidence_interval = calculate_confidence_interval(np.std(proportion_list))
+        add_elem(partial_mean_proportion_list, np.mean(proportion_list))
+        add_elem(partial_mean_waiting_max_list, np.mean(waiting_max_list))
+
+
+if __name__ == '__main__':
     config = {}
-
-    result = {
-        "N_res": [],
-        "W_mean": [],
-        "W_cis": [],
-        "TM_means": [],
-        "TM_cis": [],
-        "W_values": [],
-        "TM_values": []
-    }
-
-    with open("config.json") as f:
-        config = load(f)
+    with open('config.json', 'r') as f:
+        config = json.load(f)
     main(config)
